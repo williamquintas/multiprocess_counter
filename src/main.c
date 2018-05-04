d5c510459e46f5ecdba6669efed22315705dfc55
William Quintas de Melo <william.quintas@gmail.com>
2018-05-03 21:31:32 -0300

    Reading the numbers

--- src/main.c
+++ src/main.c
@@ -1,17 +1,16 @@
-/* Contador de palavras
- *
- * Este programa recebera uma serie de caracteres representando palavras em sua
- * entrada. Ao receber um caractere fim de linha ('\n'), deve imprimir na tela o
- * numero de palavras separadas que recebeu e, apos, encerrar.
- */
-
 #include <stdio.h>
 
+#define MAX_NUMBERS 12
+#define IN_LENGTH 100
 int main() {
-
-  int x, y;
-
-  scanf("%d %d\n", &x, &y);
-  printf("%d\n", x + 200);
+  unsigned long long int numbers[MAX_NUMBERS];
+  int array_current_size = 0;
+  char symb;
+  do {
+    scanf("%llu%c", &numbers[array_current_size++], &symb);
+  } while (symb != '\n');
+  for (size_t i = 0; i < array_current_size; i++) {
+    printf("%llu\n", numbers[i]);
+  }
   return 0;
 }
