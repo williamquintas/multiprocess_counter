2fd66392777d3814198858d7edb7b9fc0809f904
William Quintas de Melo <william.quintas@gmail.com>
2018-05-03 22:24:01 -0300

    checking prime for the first 4 entries

--- src/main.c
+++ src/main.c
@@ -8,6 +8,7 @@
 
 #define MAX_NUMBERS 12
 #define IN_LENGTH 100
+#define N_PROCESS 4
 
 int check_prime(unsigned long long int number){
   if (number <= 1 || (number != 2 && number % 2 == 0)){
@@ -26,24 +27,33 @@ int check_prime(unsigned long long int number){
 }
 
 int main() {
+  pid_t children[N_PROCESS];
   unsigned long long int numbers[MAX_NUMBERS];
   int array_current_size = 0;
   char symb;
-  /* Definir flags de protecao e visibilidade de memoria */
+  /* Define memory's protection and visibility flags */
   int protection = PROT_READ | PROT_WRITE;
   int visibility = MAP_SHARED | MAP_ANON;
-
-  /* Criar area de memoria compartilhada */
+  /* Create shared memory area */
   int *qty_primes;
   qty_primes = (int*) mmap(NULL, sizeof(int)*100, protection, visibility, 0, 0);
   if ((long int)qty_primes==-1) printf("Erro de alocacao!\n");
   (*qty_primes)=0;
-
+  /* Read the numbers */
   do {
     scanf("%llu%c", &numbers[array_current_size++], &symb);
   } while (symb != '\n');
-  for (size_t i = 0; i < array_current_size; i++) {
-    printf("%d\n", check_prime(numbers[i]));
+  /* Create the processes */
+  for (int i=0; i<N_PROCESS; i++) {
+    children[i] = fork();
+    if (children[i] == 0) {
+      (*qty_primes) += check_prime(numbers[i]);
+      exit(0);
+    }
   }
+  for (int i=0; i<N_PROCESS; i++) {
+    waitpid(children[i], NULL, 0);
+  }
+  printf("%d\n", *qty_primes);
   return 0;
 }
